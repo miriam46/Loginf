8800755c90cdf7a0cfb2976d46f90938b195b117
miriam <qaremimiriam@gmail.com>
2023-07-22 14:26:08 +0300

    login feature update

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'models/firebaseuser.dart';
+import 'package:provider/provider.dart';
+
+import 'services/auth.dart';
+import 'screens/wrapper.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+     runApp(MyApp());
+}
+ 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return StreamProvider<FirebaseUser?>.value(
+      value: AuthService().user,
+       initialData: null,
+       child: MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue,
+          textTheme: ButtonTextTheme.primary,
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(secondary: Colors.white),
+        ),
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+        ),
+      home: Wrapper(),
+    ),);
+
+  }
+}
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('fghj'),),
+      
+    );
+  }
+}
